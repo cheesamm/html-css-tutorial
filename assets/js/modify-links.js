b657ca681541438b984d8e028f02c378242dbe4d
cheesamm <jasonwongcheesam@gmail.com>
2025-02-03 17:44:49 +0800

    added modify-link.js

---
+++ assets/js/modify-links.js
@@ -0,0 +1,11 @@
+// Detect if the site is hosted on GitHub Pages
+if (window.location.hostname === "cheesamm.github.io") {
+  const repositoryName = "html-css-tutorial"; // Replace with your repository name
+  const links = document.querySelectorAll("a");
+
+  links.forEach((link) => {
+    if (link.href.startsWith("/")) {
+      link.href = "/" + repositoryName + link.getAttribute("href");
+    }
+  });
+}
