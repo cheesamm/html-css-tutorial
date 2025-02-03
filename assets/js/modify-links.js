af3016d0545d179b19a1b44be98641fe5c4da064
cheesamm <jasonwongcheesam@gmail.com>
2025-02-03 18:44:48 +0800

    Update modify-links.js

--- assets/js/modify-links.js
+++ assets/js/modify-links.js
@@ -1,18 +1,25 @@
-console.log("Hostname: " + window.location.hostname);
+// Function to modify the links on GitHub Pages
+function modifyLinksOnGithubPages() {
+  console.log("Hostname: " + window.location.hostname);
 
-// Detect if the site is hosted on GitHub Pages
-if (window.location.hostname === "cheesamm.github.io") {
-  const repositoryName = "html-css-tutorial"; // Replace with your repository name
-  const links = document.querySelectorAll("a");
-  console.log("Links count: " + links.length);
-  links.forEach((link) => {
-    if (link.href.startsWith("/")) {
-      link.href = "/" + repositoryName + link.getAttribute("href");
-      console.log("Replaced links: " + link.href);
-    } else {
-      console.log("Links that doesn't starts with / :" + link.href);
-    }
-  });
-} else {
-  console.log("Hostname different, no need to check");
+  // Detect if the site is hosted on GitHub Pages
+  if (window.location.hostname === "cheesamm.github.io") {
+    const repositoryName = "html-css-tutorial"; // Replace with your repository name
+    const links = document.querySelectorAll("a");
+    console.log("Links count: " + links.length);
+
+    links.forEach((link) => {
+      if (link.href.startsWith("/")) {
+        link.href = "/" + repositoryName + link.getAttribute("href");
+        console.log("Replaced links: " + link.href);
+      } else {
+        console.log("Links that doesn't starts with / :" + link.href);
+      }
+    });
+  } else {
+    console.log("Hostname different, no need to check");
+  }
 }
+
+// Ensure the DOM is loaded before running the function
+document.addEventListener("DOMContentLoaded", modifyLinksOnGithubPages);
