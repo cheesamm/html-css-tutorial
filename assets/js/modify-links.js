c4197a5a06279f426fb7bb199c91b7ecd4dd70e6
cheesamm <jasonwongcheesam@gmail.com>
2025-02-03 18:35:53 +0800

    Update modify-links.js

--- assets/js/modify-links.js
+++ assets/js/modify-links.js
@@ -4,11 +4,15 @@ console.log("Hostname: " + window.location.hostname);
 if (window.location.hostname === "cheesamm.github.io") {
   const repositoryName = "html-css-tutorial"; // Replace with your repository name
   const links = document.querySelectorAll("a");
-
+  console.log("Links count: " + links);
   links.forEach((link) => {
     if (link.href.startsWith("/")) {
       link.href = "/" + repositoryName + link.getAttribute("href");
-      console.log(link.href);
+      console.log("Replaced links: " + link.href);
+    } else {
+      console.log("Links that doesn't starts with / :" + link.href);
     }
   });
+} else {
+  console.log("Hostname different, no need to check");
 }
